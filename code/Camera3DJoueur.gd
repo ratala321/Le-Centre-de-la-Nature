fc314c1a45526c15c3006c0cf6e2acdec531d81f
ratala <raphael.talaia@protonmail.com>
2023-05-03 13:25:06 -0400

    Ajout de la documentation de Camera3DJoueur.gd

--- code/Camera3DJoueur.gd
+++ code/Camera3DJoueur.gd
@@ -1,10 +1,14 @@
 extends Camera3D
 
+##contient la reference au node du joueur
 @onready var joueurCanard = get_node("../JoueurCanard")
 
 func _ready():
+	#connexion au signal rotation_joueur_
 	joueurCanard.rotation_joueur_.connect(ajusterPositionCamera_rotation_joueur)
 
+##permet d'ajuster la position de la camera par rapport a la position du joueur
+##notes : cette fonction est appelee le signal [b]rotation_joueur_[/b]
 func ajusterPositionCamera_rotation_joueur(directionJoueur : Vector3) -> void:
 	position = joueurCanard.position - Vector3(0,-3,5)
 	
