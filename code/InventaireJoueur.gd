76b3a28a294a565ee2a3375d9654c9ae58d8d98d
ratala <raphael.talaia@protonmail.com>
2023-05-29 21:42:24 -0400

    Ajout capacité d'équiper objet transféré Ajout de la possibilité d'équiper un objet transféré d'un inventaire de coffre vers l'inventaire du joueur. Il s'agit d'un cas précis du fonctionnement du système. Il faut encore l'abstraire.

--- code/InventaireJoueur.gd
+++ code/InventaireJoueur.gd
@@ -9,5 +9,33 @@ func _ready():
 func selectionnerObjet(indexObjet, positionClic, indexBouttonSouris) -> void:
 	if inventaireDestination != null and inventaireDestination.visible:
 		transfererInventaire(indexObjet)
+	else:
+		var metadata = listeInventaire.get_item_metadata(indexObjet)
+		if typeof(metadata) == TYPE_OBJECT:
+			print(metadata.getDurabiliteOutils())
+			metadata.setDurabiliteOutils(metadata.getDurabiliteOutils() - 5)
+			get_parent().add_child(metadata)
+		elif typeof(metadata) == TYPE_DICTIONARY:
+			print(metadata.get("durabilite"))
+			var durabilite = metadata.get("durabilite")
+			metadata.clear()
+			durabilite = durabilite - 2
+			print(durabilite)
+			metadata = {
+				"durabilite" : durabilite
+				}
+			listeInventaire.set_item_metadata(indexObjet, metadata)
 
 
+##Permet d'evaluer si les conditions sont remplies pour qu'un objet soit
+##transfere vers un inventaire de destination.
+#func evaluerDisponibiliteTransfertObjet() -> bool:
+	#var disponible : bool = inventaireDestination != null and inventaireDestination.visible
+	#disponible = disponible and get_itemm_metadata(indexObjet).getStatusInstance()
+	#return disponible
+
+
+##Permet d'evaluer si l'objet peut etre transfere de l'inventaire
+##au joueur, autrement dit si le joueur peut equiper l'objet.
+#func evaluerDisponibiliteEquiperObjet() -> bool:
+	#return conditionDuJoueur and ConditionObjet
