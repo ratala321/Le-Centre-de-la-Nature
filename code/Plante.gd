45a7dd18071d16b1a103aa2a448b4cd32069c784
ratala <raphael.talaia@protonmail.com>
2023-06-22 12:56:14 -0400

    Ajout de la gestion de la croissance des plantes. La gestion est celle concernant les plantes elles-mêmes. Il s'agit de la méthode permettant de passer d'une étape à une autre.

--- code/Plante.gd
+++ code/Plante.gd
@@ -1,12 +1,17 @@
 extends AnimatableBody3D
 
-var dureeCroissance : int
+@export var etapesCroissance : Array
+
+const dureeCroissanceInitiale : int = 10
+
+var etapeCroissanceActuelle = 0
+var dureeCroissance : int = dureeCroissanceInitiale
 var referenceSolFertile
 
 
-func _init(dureeCroissance : int, referenceSolFertile):
-	self.dureeCroissance = dureeCroissance
-	self.referenceSolFertile = referenceSolFertile
+#func _init(dureeCroissance : int, referenceSolFertile):
+#	self.dureeCroissance = dureeCroissance
+#	self.referenceSolFertile = referenceSolFertile
 
 
 func avancerCroissance(intervalleTemps) -> void:
@@ -14,7 +19,14 @@ func avancerCroissance(intervalleTemps) -> void:
 
 	if croissanceEstFinie():
 		print("croissance " + self.name + " est finie")
+		get_node(etapesCroissance[etapeCroissanceActuelle]).hide()
+		etapeCroissanceActuelle += 1
+		get_node(etapesCroissance[etapeCroissanceActuelle]).show()
 
 
 func croissanceEstFinie() -> bool:
 	return dureeCroissance == 0
+
+
+func couperPlante() -> void:
+	pass
