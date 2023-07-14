c6374c8157d8b6f7ef8e0317b37d32e915c6b7fe
ratala <raphael.talaia@protonmail.com>
2023-07-14 07:12:39 -0400

    Séparation en classe de la détection EspacePlante

---
+++ code/DetectionEspacePlante.gd
@@ -0,0 +1,41 @@
+extends RigidBody3D
+
+
+func previsualiserPlanteDansSacGraines(aireDetectionSolFertile : Area3D, graineContenue : PackedScene):
+	var airesEnCollision = aireDetectionSolFertile.get_overlapping_areas()
+	var resultatDetection : int = _detecterEspacePlante(airesEnCollision)
+	if _espacePlanteEstDetectee(resultatDetection):
+		airesEnCollision[resultatDetection].previsualiserPlante(graineContenue)
+
+
+func _espacePlanteEstDetectee(resultatDetection : int):
+	return resultatDetection != -1
+
+
+##retourne l'index de l'espace plante detecte ou -1 lorsque l'espace plante n'est pas detecte
+func _detecterEspacePlante(airesEnCollision : Array) -> int:
+	var i : int = 0
+	var espaceNEstPasDetecte = true
+	while _detectionEstIncomplete(i, airesEnCollision.size(), espaceNEstPasDetecte):
+		espaceNEstPasDetecte = _effectuerProcedureDetection(airesEnCollision[i])
+		i += 1
+
+	if espaceNEstPasDetecte:
+		i = -1
+
+	return i
+
+
+func _detectionEstIncomplete(compteurBoucle : int,
+ nombreAiresEnCollision : int, nEstPasDetecte : bool) -> bool:
+	return compteurBoucle < nombreAiresEnCollision and nEstPasDetecte
+
+
+func _effectuerProcedureDetection(aireEnCollision) -> bool:
+		return _estEspacePlante(aireEnCollision)
+
+
+const espacePlanteGroupe : String = "EspacePlante"
+func _estEspacePlante(aireEnCollision) -> bool:
+	return aireEnCollision.is_in_group(espacePlanteGroupe)
+
--- code/SacGraines.gd
+++ code/SacGraines.gd
@@ -1,34 +1,15 @@
 extends RigidBody3D
 
-const espacePlanteGroupe : String = "EspacePlante"
-
 #load ajouté pour des tests temporaires
 var graineContenue : PackedScene = load("res://scenes/plantesScenes/BleScene.tscn")
-var aireDetectionSolFertile : Area3D
 
+var aireDetectionSolFertile : Area3D
 func _ready():
 	aireDetectionSolFertile = $AireDetectionEspacePlante
 	$IntervalleDetectionEspacesPlantes.connect("timeout", _detecterEspacePlante)
 
 
+const DetectionEspacePlante = preload("res://code/DetectionEspacePlante.gd")
+var detectionEspacePlante = DetectionEspacePlante.new()
 func _detecterEspacePlante():
-	var airesEnCollision = aireDetectionSolFertile.get_overlapping_areas()
-	
-	var i : int = 0
-	var espaceNEstPasDetecte = true
-	while _detectionEstIncomplete(i, airesEnCollision.size(), espaceNEstPasDetecte):
-		if(_estEspaceFertile(airesEnCollision[i])):
-			espaceNEstPasDetecte = false
-			print("espace fertile detecte par le sac")
-			#EspacePlante.gd
-			airesEnCollision[i].previsualiserPlante(graineContenue)
-		i += 1
-
-
-func _detectionEstIncomplete(compteurBoucle : int,
- nombreAiresEnCollision : int, nEstPasDetecte : bool) -> bool:
-	return compteurBoucle < nombreAiresEnCollision and nEstPasDetecte
-
-
-func _estEspaceFertile(aireEnCollision) -> bool:
-	return aireEnCollision.is_in_group(espacePlanteGroupe)
+	detectionEspacePlante.previsualiserPlanteDansSacGraines(aireDetectionSolFertile, graineContenue)
