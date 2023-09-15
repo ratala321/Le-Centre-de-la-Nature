71fbe4e66a072da5b5556ef7741569fbf25d6f56
ratala <raphael.talaia@protonmail.com>
2023-09-15 12:43:17 -0400

    Mise à jour objet_interactif.gd

---
+++ code/objet_interactif.gd
@@ -0,0 +1,10 @@
+class_name AbstractObjetInteractif
+extends AnimatableBody3D
+
+##reference l'aire d'interaction instanciee dans la scene
+@onready var aireInteraction : Area3D = get_node("AireInteraction")
+
+#------------------------------------
+# Permet d'effectuer une serie d'instructions liee a l'interaction associee a l'objet interactif.
+# l'interacteur doit posseder un node "Inventaire"
+# abstract effectuer_interaction(interacteur : Node) -> void:
