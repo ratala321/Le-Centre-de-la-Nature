1b6b4cc158a278271963d23481af576c30cc0aeb
ratala <raphael.talaia@protonmail.com>
2023-09-15 14:49:17 -0400

    Ajout structure sauvegarde donnees inventaire gd

---
+++ code/donnees_objet_inventaire.gd
@@ -0,0 +1,12 @@
+class_name DonneesObjetInventaire
+extends Object
+
+
+var nom_objet : String
+var chemin_scene_objet : String
+
+
+func _init(nom_objet : String, chemin_scene_objet : String):
+	self.nom_objet = nom_objet
+	self.chemin_scene_objet = chemin_scene_objet
+	
