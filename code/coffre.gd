69eb0360ab8e4f788460f403f367de917ecd5122
ratala <raphael.talaia@protonmail.com>
2023-09-16 11:26:56 -0400

    Mise à jour coffre.gd

--- code/coffre.gd
+++ code/coffre.gd
@@ -8,5 +8,19 @@ func _ready():
 
 func effecuter_interaction(interacteur : Node) -> void:
 	if interacteur.has_node("Inventaire"):
-		#TODO
-	pass
+		inventaire_coffre : AbstractInventaire = get_node("InventaireCoffre") as AbstractInventaire
+
+		_afficher_interfaces(inventaire_coffre, interacteur.inventaire_joueur)
+		_ajouter_references_inventaire_destination(inventaire_coffre, interacteur_proprietaire)
+
+
+func _afficher_interfaces(inventaire_coffre : AbstractInventaire,
+		inventaire_interacteur : AbstractInventaire) -> void:
+	inventaire_coffre.afficher_interface(self.get_tree())
+	inventaire_interacteur.afficher_interface(self.get_tree())
+
+
+func _ajouter_references_inventaire_destination(inventaire_coffre : AbstractInventaire,
+		inventaire_interacteur : AbstractInventaire) -> void:
+	inventaire_coffre.inventaire_destination = inventaire_interacteur
+	inventaire_interacteur.inventaire_destination = inventaire_coffre
