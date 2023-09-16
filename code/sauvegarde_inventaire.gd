0c5f6f4444554c71d89095c0baae7bba1d57f3ce
ratala <raphael.talaia@protonmail.com>
2023-09-15 23:28:50 -0400

    Ajout sauvegarde inventaire en Gdscript

---
+++ code/sauvegarde_inventaire.gd
@@ -0,0 +1,20 @@
+class_name SauvegardeInventaire
+extends Object
+
+
+static func sauvegarder_donnees_contenu_inventaire(donnees_a_sauvegarder : Array,
+		chemin_fichier_sauvegarde : String) -> void:
+	var redacteur_fichier : FileAccess = FileAccess.open(chemin_fichier_sauvegarde, FileAccess.WRITE)
+
+	for i in range(0, donnees_a_sauvegarder.size()):
+		_sauvegarder_donnee(redacteur_fichier, donnees_a_sauvegarder[i])
+
+	redacteur_fichier.close()
+
+
+static func _sauvegarder_donnee(redacteur_fichier : FileAccess,
+		donnee_a_sauvegarder : DonneesObjetInventaire) -> void:
+	redacteur_fichier.store_string(donnee_a_sauvegarder.nom_objet)
+	redacteur_fichier.store_string("\n")
+	redacteur_fichier.store_string(donnee_a_sauvegarder.chemin_scene_objet)
+	redacteur_fichier.store_string("\n")
