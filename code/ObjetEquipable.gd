c72ff9fa558f38b30fc18a9ba20ffd567c09713a
ratala <raphael.talaia@protonmail.com>
2023-09-17 14:27:37 -0400

    Mise à jour sac_graine.gd

--- code/ObjetEquipable.gd
+++
@@ -1,9 +0,0 @@
-class_name ObjetEquipable extends abstractInterface
-
-const methodesRequisesPourImplementation : Array = (
-		["effectuerProcedureSelection", "estObjetDeMain"]
-	)
-
-
-static func implementeInterface(objet) -> bool:
-	return verifierImplementationInterface(objet, methodesRequisesPourImplementation)
---
+++ code/sac_graine.gd
@@ -0,0 +1,57 @@
+class_name SacGraine
+extends AbstractOutils
+
+
+# load ajouté pour des tests temporaires
+var _graine_contenue : PackedScene = load("res://scenes/plantesScenes/BleScene.tscn")
+
+@onready var _aire_detection_espace_plante : Area3D = $AireDetectionEspacePlante as Area3D
+
+
+func _ready():
+	var intervalle_detection_espace_plante : Timer = $IntervalleDetectionEspacesPlantes as Timer
+
+	intervalle_detection_espace_plante.connect("timeout",
+		_effectuer_procedure_previsualisation_plante)
+
+
+func _physics_process(delta):
+	effectuer_action_principale(delta)
+
+
+func effectuer_action_principale(delta) -> void:
+	if Input.is_mouse_button_pressed(MouseButton.Right):
+		_effecuter_fonction_sur_espace_plante(Callable(self, "_ajouter_plante_dans_espace"))
+
+
+func _effectuer_procedure_previsualisation_plante() -> void:
+	_effecuter_fonction_sur_espace_plante(Callable(self, "_previsualiser_plante"))
+
+
+## Cherche un EspacePlante a portee du sac et applique la fonction sur cet EspacePlante
+## Effectuer la fonction sur le premier EspacePlante trouve
+func _effecuter_fonction_sur_espace_plante(fonction_a_appliquer : Callable)
+		var aires_detectees : Array[Area3D] =
+			_aire_detection_espace_plante.get_overlapping_areas()
+		)
+
+		var resultat_recherche : int = DetectionEspacePlante.detecter_espace_plante(aires_detectees)
+
+	if _espace_plante_est_detecte(resultat_recherche):
+		fonction_a_appliquer.call(aires_detectees[resultat_recherche])
+
+
+func _ajouter_plante_dans_espace(espace_plante_detecte : EspacePlante) -> void:
+	espace_plante_detecte.ajouter_plante_dans_espace(_graine_contenue)
+
+
+func _previsualiser_plante(espace_plante : EspacePlante) -> void:
+	espace_plante.previsualiser_plante(_graine_contenue)
+
+
+func _espace_plante_est_detecte(resultat_recherche : int) -> bool:
+	return resultat_recherche >= 0
+
+
+func est_outils_de_main() -> int:
+	return VALEUR_OUTILS_MAIN_GAUCHE 
