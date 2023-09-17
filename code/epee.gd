a51f4d8af20d170bec8dcf8a10eabc172044a813
ratala <raphael.talaia@protonmail.com>
2023-09-17 13:04:10 -0400

    Mise à jour epee.gd

---
+++ code/epee.gd
@@ -0,0 +1,15 @@
+class_name Epee
+extends AbstractOutils
+
+
+const nomEpee : = "epee"
+
+
+func est_outils_de_main() -> int:
+	return VALEUR_OUTILS_MAIN_DROITE
+
+
+func effecter_action_principale(delta) -> void:
+	#TODO
+	print("PROCEDURE EPEE")
+	pass
