45dbfecd224262fea36db499a681f95122aea7e1
ratala <raphael.talaia@protonmail.com>
2023-10-06 14:26:15 -0400

    Ajustement test_sauvegarde_inventaire.gd
    Ajustement pour le nouveau système de sauvegarde.

--- tests/test_sauvegarde_inventaire.gd
+++ tests/test_sauvegarde_inventaire.gd
@@ -1,12 +1,13 @@
 extends GutTest
 
 
+var donnees_test : Dictionary
+
 const CHEMIN_SAUVEGARDE_TEST_PARTIEL : String = "tests/test_sauvegarde_inventaire"
 const CHEMIN_SAUVEGARDE_TEST_COMPLET : String =(
 	SauvegardeInventaire.PREFIXE_USER_DIR + CHEMIN_SAUVEGARDE_TEST_PARTIEL +
 	SauvegardeInventaire.SUFFIXE_SAUVEGARDE
 )
-var donnees_test : Array[DonneesObjetInventaire] = []
 
 
 func before_all():
@@ -14,9 +15,28 @@ func before_all():
 	dir_access.make_dir("tests")
 
 
+const FORMAT_DICTIONNAIRE_TEST_1 : Dictionary = {
+		"nom_dans_inventaire" : "nom",
+		"chemin_scene_objet" : "faux_chemin_test",
+		"donnees_objet_inventaire" : {},
+	}
+const FORMAT_DICTIONNAIRE_TEST_2 : Dictionary = {
+	"nom_dans_inventaire" : "nom alternatif",
+	"chemin_scene_objet" : "vrai chemin test",
+	"donnees_objet_inventaire" : {},
+
+}
 func before_each():
-	donnees_test.push_back(DonneesObjetInventaire.new("nom de test", "faux chemin de test"))
-	donnees_test.push_back(DonneesObjetInventaire.new("nom alternatif", "vrai chemin test"))
+	var dictionnaires_donnees_test : Array = []
+
+	var dictionnaire_donnees_test_1 : Dictionary = FORMAT_DICTIONNAIRE_TEST_1
+	dictionnaires_donnees_test.push_back(dictionnaire_donnees_test_1)
+	var dictionnaire_donnees_test_2 : Dictionary = FORMAT_DICTIONNAIRE_TEST_2
+	dictionnaires_donnees_test.push_back(dictionnaire_donnees_test_2)
+
+	donnees_test = {
+		"objets_sauvegardes" : dictionnaires_donnees_test
+	}
 	
 	SauvegardeInventaire.sauvegarder_donnees_contenu_inventaire(donnees_test, CHEMIN_SAUVEGARDE_TEST_PARTIEL)
 
@@ -34,7 +54,7 @@ func test_presence_fichier_sauvegarde():
 
 
 const CONTENU_FICHIER_ATTENDU : String =(
-	"nom de test\nfaux chemin de test\nnom alternatif\nvrai chemin test\n"
+	'{"objets_sauvegardes":[{"chemin_scene_objet":"faux_chemin_test","donnees_objet_inventaire":{},"nom_dans_inventaire":"nom"},{"chemin_scene_objet":"vrai chemin test","donnees_objet_inventaire":{},"nom_dans_inventaire":"nom alternatif"}]}'
 )
 func test_contenu_fichier_sauvegarde():
 	var lecteur_fichier : FileAccess = FileAccess.open(CHEMIN_SAUVEGARDE_TEST_COMPLET, FileAccess.READ)
@@ -60,4 +80,4 @@ func test_sauvegarde_donnees_vides():
 
 	var contenu_fichier : String = lecteur_fichier.get_as_text()
 
-	assert_eq(contenu_fichier, "")
+	assert_eq(contenu_fichier, "{}")
