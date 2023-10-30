10d3515747d5a6ad7e41dbc2c34118b1e5cc0cc0
ratala <raphael.talaia@protonmail.com>
2023-10-30 11:15:52 -0400

    Ajustement classe transition 3d caméra
    La classe est dorénavant fonctionnelle.
    
    Lorsque `await` est présent dans une méthode,
    si l'on veut que le passage à la prochaine
    instruction au niveau de l'appelant se fasse
    à la suite du await, il faut place await devant
    la méthode lors de l'appel.

--- code/transition_camera_3d.gd
+++ code/transition_camera_3d.gd
@@ -4,20 +4,19 @@ extends RefCounted
 
 static func effectuer_transition_basique_camera_3d(initiale : Camera3D,
 		destination : Camera3D, duree_transition : float = 1.0) -> void:
-	var camera_de_transition : Camera3D = initialiser_camera_de_transition(initiale.get_tree())
+	var camera_de_transition : Camera3D = _initialiser_camera_de_transition(destination.get_tree())
 	
-	ajuster_parametres_initiaux_camera_transition(camera_de_transition, initiale)
+	_ajuster_parametres_initiaux_camera_transition(camera_de_transition, initiale)
 	
-	camera_de_transition.current = true
+	camera_de_transition.make_current()
 	
-	deplacer_camera_transition_vers_destination(camera_de_transition, destination, duree_transition)
+	await _deplacer_camera_transition_vers_destination(camera_de_transition, destination, duree_transition)
 	
-	destination.current = true
-	
+	destination.make_current()
 	camera_de_transition.queue_free()
 
 
-static func initialiser_camera_de_transition(arbre_scene_en_cours : SceneTree) -> Camera3D:
+static func _initialiser_camera_de_transition(arbre_scene_en_cours : SceneTree) -> Camera3D:
 	var scene_en_cours : Node = arbre_scene_en_cours.current_scene
 	
 	var camera_de_transition = Camera3D.new() 
@@ -29,7 +28,7 @@ static func initialiser_camera_de_transition(arbre_scene_en_cours : SceneTree) -
 
 ## Permet d'ajuster les parametres initiaux de la camera de transition
 ## en se basant sur la camera initale de la transition.
-static func ajuster_parametres_initiaux_camera_transition(camera_de_transition : Camera3D,
+static func _ajuster_parametres_initiaux_camera_transition(camera_de_transition : Camera3D,
 		initiale : Camera3D) -> void:
 	camera_de_transition.fov = initiale.fov
 	
@@ -38,20 +37,17 @@ static func ajuster_parametres_initiaux_camera_transition(camera_de_transition :
 	camera_de_transition.global_transform = initiale.global_transform
 
 
-static func deplacer_camera_transition_vers_destination(camera_de_transition : Camera3D,
+static func _deplacer_camera_transition_vers_destination(camera_de_transition : Camera3D,
 		destination : Camera3D, duree_transition : float) -> void:
-	var tween : Tween = Tween.new()
+	var tween : Tween = destination.create_tween()
 	
-	tween.interpolate_value(camera_de_transition.global_transform, destination.global_transform,
-		destination.get_physics_process_delta_time(), duree_transition,
-		Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+	tween.set_parallel(true)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.set_trans(Tween.TRANS_CUBIC)
 	
-	tween.interpolate_value(camera_de_transition.fov, destination.fov,
-		destination.get_physics_process_delta_time(), duree_transition, 
-		Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
-	
-	tween.play()
+	tween.tween_property(camera_de_transition, "global_transform",
+		destination.global_transform, duree_transition)
+		
+	tween.tween_property(camera_de_transition, "fov", destination.fov, duree_transition)
 	
 	await tween.finished
-	
-	tween.free()
--- tests/NiveauTest/AppelTests.gd
+++ tests/NiveauTest/AppelTests.gd
@@ -5,7 +5,7 @@ extends Node
 
 @onready var timer = $Timer
 func _ready():
-	timer.connect("timeout", testEspacePlante)
+	timer.connect("timeout", lancer_transition_camera)
 
 
 const TestEspacePlante = preload("res://tests/NiveauTest/TestEspacePlante.gd")
@@ -14,3 +14,10 @@ func testEspacePlante():
 		var testEspacePlanteInstance = TestEspacePlante.new()
 		add_child(testEspacePlanteInstance)
 		testEspacePlanteInstance.lancerTests()
+
+
+func lancer_transition_camera() -> void:
+	TransitionCamera3d.effectuer_transition_basique_camera_3d(
+		get_node("../JoueurCanard/AxeRotationCamera/Camera3DJoueur"),
+		get_node("../Destination"),
+		4.0)
