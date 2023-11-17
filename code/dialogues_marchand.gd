fbaf8a22352ea8044f2f30d5cf5c90cb46a567d2
ratala <raphael.talaia@protonmail.com>
2023-11-17 01:16:20 -0500

    Ajout boutton quitter dialogues marchand
    Permet le retour en jeu

--- code/dialogues_marchand.gd
+++ code/dialogues_marchand.gd
@@ -2,16 +2,32 @@ class_name DialoguesMarchand
 extends Node
 
 
+func _ready():
+	%ButtonQuitter.connect("pressed", _effectuer_procedure_cacher_interface_dialogue)
+
+
 func afficher_dialogues_marchand() -> void:
 	$InterfaceUtilisateur.visible = true
 
 	_lancer_transition_affichage_ui()
+
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
 
 
 func cacher_dialogues_marchand() -> void:
 	$InterfaceUtilisateur.visible = false
 
 	_lancer_transition_cacher_ui()
+
+
+signal boutton_quitter_clique
+
+func _effectuer_procedure_cacher_interface_dialogue() -> void:
+	cacher_dialogues_marchand()
+
+	emit_signal("boutton_quitter_clique")
+
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 const OFFSET_BAS_ECRAN_Y : int = 400
@@ -26,8 +42,6 @@ func _lancer_transition_affichage_ui() -> void:
 	tween.set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property(self, "offset", Vector2(self.offset.x, OFFSET_MILIEU_ECRAN_Y), 1.0)
 
-	await tween.finished
-
 
 func _lancer_transition_cacher_ui() -> void:
 	var tween : Tween = get_tree().create_tween()
@@ -35,5 +49,3 @@ func _lancer_transition_cacher_ui() -> void:
 	self.offset.y = OFFSET_MILIEU_ECRAN_Y
 
 	tween.tween_property(self, "offset", Vector2(self.offset.x, OFFSET_BAS_ECRAN_Y), 1.0)
-
-	await tween.finished
--- code/marchand.gd
+++ code/marchand.gd
@@ -5,18 +5,17 @@ extends Node3D
 ## Angle de vue, lors du marchandage avec le joueur, etant placee dans la scene du niveau.
 @export var angle_vue_marchand : Camera3D
 
-## Temps de transition de la camera en cours vers l'angle de vue.
-@export var temps_transition_angle_vue : float = 1.5
+## Temps de transition de la camera en cours vers l'angle de vue[br]
+## ou de l'angle de vue vers la camera du joueur.
+@export var temps_transition_cameras : float = 1.5
 
 
 func _ready():
-	set_process_mode(PROCESS_MODE_WHEN_PAUSED)
+	var dialogues_marchand = get_node("DialoguesMarchand")
+	dialogues_marchand.connect("boutton_quitter_clique", _liberer_joueur_contraintes_interaction)
 
 
 func _physics_process(delta):
-	if Input.is_key_pressed(KEY_Q) and get_node("DialoguesMarchand").visible:
-		_effectuer_procedure_cacher_interface_dialogue()
-
 	if client_en_cours:
 		client_en_cours.progresser_animation_joueur(delta)
 
@@ -28,13 +27,25 @@ var camera_joueur : Camera3D
 func effectuer_interaction_initiale_avec_joueur(joueur : JoueurCanard) -> void:
 	client_en_cours = joueur
 
+	_contraindre_joueur_pour_interaction(joueur)
+
+	get_node("DialoguesMarchand").afficher_dialogues_marchand()
+
+
+## Libere le joueur des contraintes etablies pour permettre une interaction fluide avec le marchand.
+func _liberer_joueur_contraintes_interaction() -> void:
+	await _lancer_transition_vers_camera_joueur()
+
+	client_en_cours.relancer_mouvement()
+
+
+## Etablit des contraintes au joueur afin de permettre une interaction fluide avec le marchand.
+func _contraindre_joueur_pour_interaction(joueur : JoueurCanard) -> void:
 	_immobiliser_joueur(joueur)
 
 	camera_joueur = _obtenir_camera_joueur(joueur)
 
 	_lancer_transition_vers_angle_de_vue(camera_joueur)
-
-	_effectuer_procedure_afficher_interface_dialogue()
 
 
 func _immobiliser_joueur(joueur : JoueurCanard) -> void:
@@ -44,25 +55,21 @@ func _immobiliser_joueur(joueur : JoueurCanard) -> void:
 	joueur.arreter_mouvement()
 
 
-func _obtenir_camera_joueur(joueur : JoueurCanard) -> Camera3D:
-	return joueur.camera_joueur
+func _lancer_transition_vers_camera_joueur() -> void:
+	await TransitionCamera3d.effectuer_transition_basique_camera_3d(
+		angle_vue_marchand,
+		camera_joueur,
+		temps_transition_cameras / 2
+	)
 
 
 func _lancer_transition_vers_angle_de_vue(en_cours_utilisation : Camera3D) -> void:
 	TransitionCamera3d.effectuer_transition_basique_camera_3d(
 			en_cours_utilisation,
 			angle_vue_marchand,
-			temps_transition_angle_vue
+			temps_transition_cameras
 	)
 
 
-func _effectuer_procedure_afficher_interface_dialogue() -> void:
-	get_node("DialoguesMarchand").afficher_dialogues_marchand()
-
-	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
-
-
-func _effectuer_procedure_cacher_interface_dialogue() -> void:
-	get_node("DialoguesMarchand").cacher_dialogues_marchand()
-
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+func _obtenir_camera_joueur(joueur : JoueurCanard) -> Camera3D:
+	return joueur.camera_joueur
