5dfcac140d66f87a54569a92b887c4df4084272a
ratala <talaia.gamedev@pm.me>
2023-11-24 17:13:03 -0500

    Ajustement commentaire documentation

--- code/inventaire_coffre.gd
+++ code/inventaire_coffre.gd
@@ -8,8 +8,8 @@ func ready_instance_inventaire() -> void:
 	set_process_mode(PROCESS_MODE_WHEN_PAUSED)
 
 
-##permet d'effectuer le transfert d'un objet d'un coffre
-##vers l'inventaire de destination
+## Permet d'effectuer le transfert d'un objet d'un coffre
+## vers l'inventaire de destination
 func effectuer_procedure_selection_objet(
 		index_objet, _position_clic,_index_boutton_souris) -> void:
 	transferer_objet_vers_inventaire_destination(index_objet)
